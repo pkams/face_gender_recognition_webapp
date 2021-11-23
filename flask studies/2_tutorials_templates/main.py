ca6c164c096f3373e3da5e1cfc9ccd3716f59828
pkams <patrick_ams@hotmail.com>
2021-11-23 10:45:30 -0300

    refactor some folders names

--- flask studies/2_tutorials_templates/main.py
+++ flask studies/2_tutorials_templates/main.py
@@ -8,7 +8,7 @@ app.add_url_rule('/', 'index', views.index)
 app.add_url_rule('/templates/<name>/<int:marks>', 'index_template', views.index_tem)
 app.add_url_rule('/template_for', 'template_for', views.index_tem_for)
 
-# run the flask app
+# run the flask studies app
 
 if __name__ == '__main__':
     app.run(debug=True)
